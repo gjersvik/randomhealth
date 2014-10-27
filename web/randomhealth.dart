d92f031c7e2c130ab7276a286b3a2654ec9aafea
Ole Martin Gjersvik <olem@olem.org>
2014-10-27 20:22:22 +0100

    Random pick

--- web/randomhealth.dart
+++ web/randomhealth.dart
@@ -3,5 +3,11 @@ import 'package:firebase/firebase.dart';
 
 void main() {
   var fire = new Firebase('popping-inferno-887.firebaseio.com/store/fruit');
-  fire.onValue.listen((e) => print(e.snapshot.val()));
+  fire.onValue.listen((e) => print(pic3(e.snapshot.val())));
+}
+
+List pic3(Map data){
+  var validKeys = data.keys.where((name) => data[name] > 0).toList();
+  validKeys.shuffle();
+  return validKeys.take(3).toList();
 }
