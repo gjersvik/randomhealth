f08a00f4c718df8da438c87b535fa6543ebb5206
Ole Martin Gjersvik <olem@olem.org>
2014-11-20 16:23:48 +0100

    Resepe gets getters

--- web/src/recipe.dart
+++ web/src/recipe.dart
@@ -1,19 +1,31 @@
 part of randomhealth;
 
 class Recipe{
+  Store _store;
   
-  Recipe(Store store){
-    var list = store.catergory['veg'].pick(2);
-    querySelector('#breakfast .ingredients').innerHtml = toHtml(list);
-    
-    list = store.catergory['fruit'].pick(2);
-    querySelector('#afternoon .ingredients').innerHtml = toHtml(list);
-    
-    list = ['meat','gravy','veg'].map((cat) => store.catergory[cat].pick(1).single).toList();
-    querySelector('#dinner .ingredients').innerHtml = toHtml(list);
+  Recipe(this._store){
+    querySelector('#breakfast .ingredients').innerHtml = toHtml(getBreakfast());
+    querySelector('#dinner .ingredients').innerHtml = toHtml(getDinner());
+    querySelector('#afternoon .ingredients').innerHtml = toHtml(getAfternoon());
   }
   
   String toHtml(List<Ingredient> ingedience){
     return ingedience.map((i) => '<div class="${i.type}">${i.name} 1 av ${i.inStore}</div>').join('');
+  }
+  
+  List<Ingredient> getBreakfast(){
+    var list = _store.catergory['veg'].pick(2);
+    list.addAll(_store.catergory['gravy'].pick(1));
+    return list;
+  }
+  
+  List<Ingredient> getDinner(){
+    var list = _store.catergory['meat'].pick(1);
+    list.addAll(_store.catergory['veg'].pick(1));
+    return list;
+  }
+  
+  List<Ingredient> getAfternoon(){
+    return _store.catergory['fruit'].pick(2);
   }
 }
